51d335ed04c7f97390b53316a5f011c5b7731ea9
remco1250 <remco.klinckenberg@hotmail.nl>
2016-04-07 14:01:21 +0200

    Fixed TribotActivity

--- app/src/main/java/com/hszuyd/noodle_/testing/TribotActivity.java
+++ app/src/main/java/com/hszuyd/noodle_/testing/TribotActivity.java
@@ -1,22 +1,26 @@
 package com.hszuyd.noodle_.testing;
 
-import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+
 
 public class TribotActivity extends AppCompatActivity {
-
-	//private final static int REQUEST_ENABLE_BT = 1; //TODO figure out what this number means..
-	BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-	//Bluetooth bluetooth = new Bluetooth();
-	boolean isOn = false;
+	private static final String TAG = "KickPanelActivity";
+	private static final int REQUEST_DEVICE_ADDRESS = 1;
+	BluetoothSPP bt = new BluetoothSPP(TribotActivity.this);
+	private TextView textView;
 	MenuItem mDynamicMenuIcon;
-	//TODO fix the icon changing, often won't work properly.
-	int iconID = R.drawable.ic_bluetooth;
-	private Menu mMenu;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,85 +28,111 @@ public class TribotActivity extends AppCompatActivity {
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		/*new Thread(new Runnable() {
-			public void run() {
+		textView = (TextView) findViewById(R.id.textView2);
+		Intent iin = getIntent();
+		Bundle b = iin.getExtras();
 
-				mImageView.post(new Runnable() {
-					public void run() {
-						mImageView.setImageBitmap(bitmap);
-					}
-				});
-			}
-		}).start();*/
+		if (b != null) {
+			String device_Address = (String) b.get("EXTRA_DEVICE_ADDRESS");
+			textView.setText("Device: " + device_Address);
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu, menu);
 		mDynamicMenuIcon = menu.findItem(R.id.tb_bluetooth);
-		if(mBluetoothAdapter.isEnabled()){
-			mDynamicMenuIcon.setIcon(R.drawable.ic_bluetooth_disabled);
-		}
-		else{
-			mDynamicMenuIcon.setIcon(R.drawable.ic_bluetooth);
-		}
+		mDynamicMenuIcon.setIcon(R.drawable.ic_favorite_white);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
+		int id = item.getItemId();      // get ID of the clicked button so we know where we've clicked and act based on that
 		if (id == R.id.tb_bluetooth) {
-			//bluetooth.tb_OnOff();
-			if (iconID == R.drawable.ic_bluetooth_disabled && mBluetoothAdapter.isEnabled()) {
-				item.setIcon(R.drawable.ic_bluetooth);
-				iconID = R.drawable.ic_bluetooth;
+			if (Math.random() > 0.5) {  // Show a different icon based on random int between 0.0 and 1.0
+				mDynamicMenuIcon.setIcon(R.drawable.ic_thumb_up);
 			} else {
-				item.setIcon(R.drawable.ic_bluetooth_disabled);
-				iconID = R.drawable.ic_bluetooth_disabled;
-				//startActivity(new Intent(this, Bluetooth.class));
+				mDynamicMenuIcon.setIcon(R.drawable.ic_thumb_down);
 			}
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
-	/*public void tb_BT_OnOff(MenuItem item) {
-		bluetooth.tb_OnOff();
-		if(mBluetoothAdapter != null){
-			if(!mBluetoothAdapter.isEnabled()){
-				mBluetoothAdapter.enable();
-				Snackbar.make(this.findViewById(android.R.id.content), "Enabling Bluetooth", Snackbar.LENGTH_LONG).show();
-			}
-			else if(mBluetoothAdapter.isEnabled()){
-				mBluetoothAdapter.disable();
-				Snackbar.make(this.findViewById(android.R.id.content), "Bluetooth is disabled", Snackbar.LENGTH_LONG).show();
-			}
-		}
-		//This is here for test purposes only.
-		else{
-			new AlertDialog.Builder(this)
-					.setTitle("Not compatible")
-					.setMessage("Your phone does not support Bluetooth")
-					.setPositiveButton("Exit", new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int which) {
-							System.exit(0);
-						}
-					})
-					.setIcon(android.R.drawable.ic_dialog_alert)
-					.show();
-		}
-	}*/
+	public void button_start_device_list(View v) {
+		startActivityForResult(new Intent(this, DeviceListActivity.class), REQUEST_DEVICE_ADDRESS);
+	}
 
-	/*public void button_changeBluetoothIcon_OnClick() {
-		MenuItem item = mMenu.findItem(R.id.tb_bluetooth);
-		if (mMenu != null) {
-			if (isOn = false) {
-				item.setIcon(R.drawable.ic_bluetooth_disabled);
-				isOn = true;
-			} else {
-				item.setIcon(R.drawable.ic_bluetooth);
-				isOn = false;
+	public void button_Random_Math_Number(View v) {
+		textView = (TextView) findViewById(R.id.textView);
+		String rndmstring = String.valueOf(Math.random());
+		textView.setText(rndmstring);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_DEVICE_ADDRESS) {        // Check which request we're responding to. When doing more requests a switch case is probably a nicer way of doing this.
+			if (resultCode == RESULT_OK) {                  // Make sure the request was successful
+				if (data.hasExtra("EXTRA_DEVICE_ADDRESS")) {
+					Bundle bundleResult = data.getExtras(); // Store the Intent data(=device address) that we've received from the DeviceListActivity in a bundle. The bundle consists of "EXTRA_DEVICE_ADDRESS, MAC_ADDRESS"
+					String device = bundleResult.getString("EXTRA_DEVICE_ADDRESS");
+
+					textView = (TextView) findViewById(R.id.textView2);
+					textView.setText("Device address: " + device);// Make sure the request was successful
+
+					Log.e(TAG, "SetupService()");
+					bt.setupService();
+					startBluetoothService();
+
+					Log.e(TAG, "Connecting to " + device);
+					Toast.makeText(getApplicationContext(), "Connecting to " + device, Toast.LENGTH_SHORT).show();    //TODO Remove this when we've successfully sent through the address
+					bt.connect(device);
+				} else {
+					Toast.makeText(getApplicationContext(), "Failed to get MAC address from ", Toast.LENGTH_SHORT).show();    //TODO Remove this when we've successfully sent through the address
+				}
 			}
 		}
-	}*/
+	}
+
+	private void startBluetoothService() {
+		//For connection with android device
+		bt.startService(BluetoothState.DEVICE_ANDROID);
+		// TODO Find a way to do this automatically
+		//For connection with any micro-controller which communication with bluetooth serial port profile module
+		//bt.startService(BluetoothState.DEVICE_OTHER);
+
+		bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+			public void onDataReceived(byte[] data, String message) {
+				Log.e(TAG, "OnDataReceivedListener ->\ndata " + data + "\nmessage " + message);
+			}
+		});
+
+		bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() {
+			public void onDeviceConnected(String name, String address) {
+				Log.e(TAG, "BluetoothConnectionListener -> onDeviceConnected" + "\nname: " + name + "\taddress " + address);
+			}
+
+			public void onDeviceDisconnected() {
+				Log.e(TAG, "BluetoothConnectionListener -> onDeviceDisconnected");
+			}
+
+			public void onDeviceConnectionFailed() {
+				Log.e(TAG, "BluetoothConnectionListener -> onDeviceConnectionFailed");
+			}
+		});
+
+		bt.setBluetoothStateListener(new BluetoothSPP.BluetoothStateListener() {
+			public void onServiceStateChanged(int state) {
+				if (state == BluetoothState.STATE_CONNECTED) {
+					Log.e(TAG, "BluetoothStateListener -> STATE_CONNECTED");
+				} else if (state == BluetoothState.STATE_CONNECTING) {
+					Log.e(TAG, "BluetoothStateListener -> STATE_CONNECTING");
+				} else if (state == BluetoothState.STATE_LISTEN) {
+					Log.e(TAG, "BluetoothStateListener -> STATE_LISTEN");
+				} else if (state == BluetoothState.STATE_NONE) {
+					Log.e(TAG, "BluetoothStateListener -> STATE_NONE");
+				}
+			}
+		});
+	}
 }

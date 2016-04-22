c57e2487718f17fb6cef6a95d9ffb8c6e72d76ae
Nils Bünger <menomnomspam@live.com>
2016-04-22 10:21:51 +0200

    Improve checkName by using regex matching Change accent color Change MainActivity button size Change login input type and error message

--- app/src/main/java/com/hszuyd/noodle_/testing/LoginActivity.java
+++ app/src/main/java/com/hszuyd/noodle_/testing/LoginActivity.java
@@ -1,16 +1,16 @@
 package com.hszuyd.noodle_.testing;
 
 import android.app.ActivityOptions;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
-import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 	General g = new General(LoginActivity.this);
@@ -21,7 +21,6 @@ public class LoginActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_login_screen);
 		Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(mToolbar);
-
 		g.checkBluetooth();
 	}
 
@@ -29,37 +28,27 @@ public class LoginActivity extends AppCompatActivity {
 		EditText text = (EditText) findViewById(R.id.editText);
 		String name = text.getText().toString().trim(); // Remove trailing spaces
 
-//		if (checkName(name)) {                          // TODO Remove this when not testing.
-		// Start MainActivity and give it "name" through an intent
-		Intent intent = new Intent(getBaseContext(), MainActivity.class);       // Create intent for going from here to KickPanel
-		intent.putExtra("NAME_PLAYER", name);                                   // Add player name to the intent as Extra
-		ActivityOptions transitionActivityOptions =                             // Set animation options
-				ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, text, "ToMain");
-		startActivity(intent, transitionActivityOptions.toBundle());            // Start new activity with player name and animation
-//		}
+		if (checkName(name)) {                          // TODO Remove this when not testing.
+			Intent intent = new Intent(getBaseContext(), MainActivity.class);       // Create intent for going from here to KickPanel
+			intent.putExtra("NAME_PLAYER", name);                                   // Add player name to the intent as Extra
+			ActivityOptions transitionActivityOptions =                             // Set animation options
+					ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, text, "ToMain");
+			startActivity(intent, transitionActivityOptions.toBundle());            // Start new activity with player name and animation
+		} else {
+			TextInputLayout til = (TextInputLayout) findViewById(R.id.nameWrapper);
+			til.setErrorEnabled(true);
+			til.setError("Please enter a valid name");   // Show a hint to indicate that it was false input}
+		}
 	}
 
 	/**
 	 * Check whether the user has entered a name. Show a dialog if he has not.
 	 */
 	private boolean checkName(String name) {
-		if (Objects.equals(name, "")) {
-			new AlertDialog.Builder(this)
-					.setTitle("Invalid name")
-					.setMessage("Please insert your name.")
-					.setCancelable(true)
-					.setPositiveButton(
-							"Okay",
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog, int id) {
-									dialog.cancel();
-								}
-							})
-					.setIcon(R.drawable.ic_warning)
-					.show();
-			return false;
-		} else {
-			return true;    // User has entered a valid name, so we're sending his name to the next activity
-		}
+		final String VALID_NAME = "[a-zA-Z0-9 ]+";          // Regex string to make the user choose a name with at least one character, only including numbers and letters
+		Pattern pattern = Pattern.compile(VALID_NAME);
+		Matcher matcher;
+		matcher = pattern.matcher(name);                    // Compare regex string to name
+		return matcher.matches();                           // Return whether the name matches the regex pattern
 	}
 }

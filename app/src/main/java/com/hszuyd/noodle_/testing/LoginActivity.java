7202623c23a7bee0555d622c76fc6c866cd7f524
Nils Bünger <menomnomspam@live.com>
2016-04-22 11:14:38 +0200

    Improve TextInputLayout

--- app/src/main/java/com/hszuyd/noodle_/testing/LoginActivity.java
+++ app/src/main/java/com/hszuyd/noodle_/testing/LoginActivity.java
@@ -26,24 +26,26 @@ public class LoginActivity extends AppCompatActivity {
 
 	public void buttonStartAppOnClick(View view) {
 		EditText text = (EditText) findViewById(R.id.editText);
+		TextInputLayout mInputName = (TextInputLayout) findViewById(R.id.nameWrapper);
 		String name = text.getText().toString().trim(); // Remove trailing spaces
 
 		if (checkName(name)) {                          // TODO Remove this when not testing.
+			//mInputName.setErrorEnabled(false);                                             // Set the textView to non error layout in case the users input was wrong before
 			Intent intent = new Intent(getBaseContext(), MainActivity.class);       // Create intent for going from here to KickPanel
 			intent.putExtra("NAME_PLAYER", name);                                   // Add player name to the intent as Extra
 			ActivityOptions transitionActivityOptions =                             // Set animation options
 					ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, text, "ToMain");
 			startActivity(intent, transitionActivityOptions.toBundle());            // Start new activity with player name and animation
 		} else {
-			TextInputLayout til = (TextInputLayout) findViewById(R.id.nameWrapper);
-			til.setErrorEnabled(true);
-			til.setError("Please enter a valid name");   // Show a hint to indicate that it was false input}
+			mInputName.setErrorEnabled(true);
+			mInputName.setError("Please enter a valid name");   // Show a hint to indicate that it was false input}
 		}
 	}
 
 	/**
 	 * Check whether the user has entered a name. Show a dialog if he has not.
 	 */
+
 	private boolean checkName(String name) {
 		final String VALID_NAME = "[a-zA-Z0-9 ]+";          // Regex string to make the user choose a name with at least one character, only including numbers and letters
 		Pattern pattern = Pattern.compile(VALID_NAME);

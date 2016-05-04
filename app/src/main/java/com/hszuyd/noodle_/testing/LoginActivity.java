ac5e67079171684c297fda5a25c624b443ddf129
Nils Bünger <menomnomspam@live.com>
2016-05-04 12:58:20 +0200

    Update app icon Fix LoginActivity xml names

--- app/src/main/java/com/hszuyd/noodle_/testing/LoginActivity.java
+++ app/src/main/java/com/hszuyd/noodle_/testing/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login_screen);
+		setContentView(R.layout.activity_login);
 		Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(mToolbar);
 		g.checkBluetooth();

54050cb1f25812d55a7834a14a282c0ee181750a
Nils Bünger <menomnomspam@live.com>
2016-06-06 15:13:19 +0200

    PreferencesActivity including PreferenceFragment and showing the currently selected option
    This commit includes a lot of rubbish that will have to be cleaned up in
    future commits

--- app/src/main/java/com/hszuyd/bluetoothremote/TribotNewActivity.java
+++ app/src/main/java/com/hszuyd/bluetoothremote/TribotNewActivity.java
@@ -2,8 +2,6 @@ package com.hszuyd.bluetoothremote;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,6 +16,7 @@ public class TribotNewActivity extends AppCompatActivity {
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 	}
+
 	public void showSettings(View view) {
 		Intent intent = new Intent(getBaseContext(), SettingsActivity.class);
 		startActivity(intent);

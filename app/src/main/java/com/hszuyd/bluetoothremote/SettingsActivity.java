044876c575084000dd93b40c3dbedb654facd6e7
Nils Bünger <menomnomspam@live.com>
2016-06-08 16:43:16 +0200

    Small improvements after trying to rewrite it all.

--- app/src/main/java/com/hszuyd/bluetoothremote/SettingsActivity.java
+++ app/src/main/java/com/hszuyd/bluetoothremote/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class SettingsActivity extends AppCompatActivity {
 	@Override
@@ -28,24 +29,24 @@ public class SettingsActivity extends AppCompatActivity {
 		@Override
 		public void onCreate(final Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.preferences);                          // Load the preferences layout
-			findPreference("pref_leesplankje").setOnPreferenceChangeListener(this); // Initialize the change listener
-			findPreference("pref_display_name").setOnPreferenceChangeListener(this); // Initialize the change listener
+			addPreferencesFromResource(R.xml.preferences);                              // Load the preferences layout
+			findPreference("pref_leesplankje").setOnPreferenceChangeListener(this);     // Initialize the change listener
+			findPreference("pref_display_name").setOnPreferenceChangeListener(this);    // Initialize the change listener
 		}
 
 		@Override
 		public boolean onPreferenceChange(Preference pref, Object newValue) {
 			if (pref instanceof ListPreference) {
-				SharedPreferences SharedPref = getActivity().getSharedPreferences("pref_leesplankje", MODE_PRIVATE);   // TODO not sure about using "pref_leesplankje" right here, as it does not seem to matter, at all.
+				SharedPreferences SharedPref = getActivity().getSharedPreferences("pref_leesplankje", MODE_PRIVATE);   // Load the preference TODO not sure about using "pref_leesplankje" right here, as it does not seem to matter, at all.
 				SharedPref.edit().putString("pref_leesplankje", newValue.toString()).apply();   // Store the new value.
 				findPreference("pref_leesplankje").setSummary((String) newValue);               // Set the summary to the new value
 				return true;
 			}
-
-			if (pref instanceof EditTextPreference) {
-				SharedPreferences SharedPref = getActivity().getSharedPreferences("pref_display_name", MODE_PRIVATE);
+			if (pref instanceof EditTextPreference) {                                           // TODO: This does not work, I don't know why.
+				SharedPreferences SharedPref = getActivity().getSharedPreferences("pref_display_name", MODE_PRIVATE); // Load the preference
 				SharedPref.edit().putString("pref_display_name", newValue.toString()).apply();  // Store the new value.
 				findPreference("pref_display_name").setSummary((String) newValue);              // Set the summary to the new value
+				Log.e("asdf", "Summary should be updated...");
 				return true;
 			}
 			return false;
